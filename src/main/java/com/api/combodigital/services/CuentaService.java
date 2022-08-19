8571c4445f4660ed8a2cc52dd604c2039d2f8714
Giorman Rodriguez <100052544+giorman@users.noreply.github.com>
2022-08-18 23:45:00 -0500

    cambios en los servicios

--- src/main/java/com/api/combodigital/services/CuentaService.java
+++ src/main/java/com/api/combodigital/services/CuentaService.java
@@ -4,6 +4,7 @@ import com.api.combodigital.entities.Cuenta;
 import com.api.combodigital.repositories.CuentaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -14,26 +15,31 @@ public class CuentaService implements ICuentaService {
     CuentaRepository cuentaRepository;
 
     @Override
+    @Transactional
     public Collection<Cuenta> listaCuenta() {
         return cuentaRepository.findAll();
     }
 
     @Override
+    @Transactional
     public Optional<Cuenta> consultarCuenta(Long id) {
         return cuentaRepository.findById(id);
     }
 
     @Override
+    @Transactional
     public Cuenta agregarCuenta(Cuenta cuenta) {
         return cuentaRepository.save(cuenta);
     }
 
     @Override
+    @Transactional
     public Cuenta editarCuenta(Cuenta cuenta) {
         return cuentaRepository.save(cuenta);
     }
 
     @Override
+    @Transactional
     public void eliminar(Long id) {
 
         cuentaRepository.deleteById(id);
--- src/main/java/com/api/combodigital/services/GananciaService.java
+++ src/main/java/com/api/combodigital/services/GananciaService.java
@@ -4,6 +4,7 @@ import com.api.combodigital.entities.Ganancia;
 import com.api.combodigital.repositories.GananciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Collection;
@@ -16,26 +17,31 @@ public class GananciaService implements IGananciaService{
     private GananciaRepository gananciaRepository;
 
     @Override
+    @Transactional
     public Collection<Ganancia> listaGanancia() {
         return gananciaRepository.findAll();
     }
 
     @Override
+    @Transactional
     public Optional<Ganancia> buscarGanancia(long id) {
         return gananciaRepository.findById(id);
     }
 
     @Override
+    @Transactional
     public Ganancia agregarGanancia(Ganancia ganancia) {
         return gananciaRepository.save(ganancia);
     }
 
     @Override
+    @Transactional
     public Ganancia editarGanancia(Ganancia ganancia) {
         return gananciaRepository.save(ganancia);
     }
 
     @Override
+    @Transactional
     public Collection<Ganancia> editarValorGanancia(Double valor) {
         gananciaRepository.findAll().forEach((ganancia -> {
             if (ganancia.getId()== LocalDate.now().getMonthValue()){
@@ -47,6 +53,7 @@ public class GananciaService implements IGananciaService{
     }
 
     @Override
+    @Transactional
     public void eliminarGanancia(Long id) {
             gananciaRepository.deleteById(id);
     }
